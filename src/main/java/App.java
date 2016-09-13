5179361ea0b9528aea5a15d651e5a91ef7fb6e4a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-12 18:05:37 -0700

    connect game object to spark template, style output

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,37 @@
-import java.io.Console;
+import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 
 public class App {
   public static void main(String[] args) {
-    Console c = System.console();
+    staticFileLocation("/public");
     Game newGame = new Game();
-    System.out.println("Player one please enter rock, paper or scissors ");
-    String playerOneChoice = c.readLine();
-    System.out.println("Player two please enter rock, paper or scissors ");
-    String playerTwoChoice = c.readLine();
-    if(newGame.rockWins(playerOneChoice, playerTwoChoice)&&!newGame.isTie(playerOneChoice, playerTwoChoice)) {
-      System.out.println("Rock wins!!!");
-    } else if(newGame.scissorsWin(playerOneChoice, playerTwoChoice)&&!newGame.isTie(playerOneChoice, playerTwoChoice)) {
-      System.out.println("Scissors wins!!!");
-    } else if(newGame.paperWin(playerOneChoice, playerTwoChoice)&&!newGame.isTie(playerOneChoice, playerTwoChoice)) {
-      System.out.println("Paper wins!!!");
-    }
+    String[] rps = {"rock","paper","scissors"};
+
+    get("/", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/input.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
+    get("/rps", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int player1 = Integer.parseInt(request.queryParams("player_1"));
+      String playerOneChoice = rps[player1];
+      int player2 = Integer.parseInt(request.queryParams("player_2"));
+      String playerTwoChoice = rps[player2];
+
+      if(newGame.rockWins(playerOneChoice, playerTwoChoice)&&!newGame.isTie(playerOneChoice, playerTwoChoice)) {
+        model.put("whoWon", "Rock wins!!!");
+      } else if(newGame.scissorsWin(playerOneChoice, playerTwoChoice)&&!newGame.isTie(playerOneChoice, playerTwoChoice)) {
+        model.put("whoWon", "Scissors wins!!!");
+      } else if(newGame.paperWin(playerOneChoice, playerTwoChoice)&&!newGame.isTie(playerOneChoice, playerTwoChoice)) {
+        model.put("whoWon", "Paper wins!!!");
+      }
+      model.put("template", "templates/results.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
   }
 }
